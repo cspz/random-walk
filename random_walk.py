63966b474df5a9e45f69d2ef0617ae31a3092f54
cspz <pietrozito98@gmail.com>
2026-02-20 16:22:11 +0100

    added comments and descriptive statistics of the generated array with pandas

--- random_walk.py
+++ random_walk.py
@@ -1,19 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random as rnd
+import pandas as pd
 
+
+print("="*5+" Initial tests "+"="*6)
+# random.random() generates a random float between 0.0 and 1.0 (inclusive)
 test = rnd.random()
 print("test random number:", f"{test:.2f}")
 
-i =0
+
 arr = np.array([])
-print(arr)
-while i < 10:
-    test = rnd.random()
-    print(test)
+
+s = rnd.sample(range(1, 100), 10)
+print("Random sample of 10 numbers between 1 and 100:", s)
+
+print("="*20)
+
+print("="*5+" Random walk "+"="*5)
+
+# random.uniform(a,b) generates a random float between a and b (inclusive)
+i=0
+while i < 1000:
+    test = rnd.uniform(-5,5)
     arr = np.append(arr,test)
     i += 1
-print(arr)
+
 
 len(arr)
 x_values = np.array([])
@@ -26,7 +38,11 @@ print("Standard deviation of array:", f"{arr_stf:.2f}")
 
 for i in range(len(arr)):
     x_values = np.append(x_values,i)
-print(x_values)
+
+
+describe = pd.DataFrame(arr).describe()
+print("Descriptive statistics of the array:")
+print(describe)
 
 plt.plot(x_values, arr)
 plt.axhline(y=arr_mean, color='red', linestyle='--', label=f'Mean: {arr_mean:.2f}')
