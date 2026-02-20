0741c5e21ef5d2a37413167ebf26c1c8d0813288
cspz <pietrozito98@gmail.com>
2026-02-20 16:11:22 +0100

    added mean and std, added red line corresponding to the mean value in the plot

--- random_walk.py
+++ random_walk.py
@@ -2,10 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random as rnd
 
-
 test = rnd.random()
-print(test)
-
+print("test random number:", f"{test:.2f}")
 
 i =0
 arr = np.array([])
@@ -20,14 +18,22 @@ print(arr)
 len(arr)
 x_values = np.array([])
 
+arr_mean = np.mean(arr)
+print("Mean of array:", f"{arr_mean:.2f}")
+
+arr_stf = np.std(arr)
+print("Standard deviation of array:", f"{arr_stf:.2f}")
 
 for i in range(len(arr)):
     x_values = np.append(x_values,i)
-
-
 print(x_values)
 
 plt.plot(x_values, arr)
+plt.axhline(y=arr_mean, color='red', linestyle='--', label=f'Mean: {arr_mean:.2f}')
+plt.legend()
 plt.show()
+
+
+
 
 
