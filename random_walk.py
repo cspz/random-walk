4ce288a6a6a9307d759d9db3e49be35a774e2c67
cspz <pietrozito98@gmail.com>
2026-02-20 15:57:46 +0100

    converted python script random_walk to normal python script from notebook format. Added imports at the top and removed the cell markers. Also added a plot at the end to visualize the random walk.

--- random_walk.py
+++ random_walk.py
@@ -1,47 +1,12 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.19.1
-#   kernelspec:
-#     display_name: base
-#     language: python
-#     name: python3
-# ---
-
-# %%
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.19.1
-
-#   kernelspec:
-#     display_name: base
-#     language: python
-#     name: python3
-# ---
-
-# %%
-import random as rnd
 import numpy as np
 import matplotlib.pyplot as plt
+import random as rnd
 
 
-
-# %%
 test = rnd.random()
 print(test)
 
-# %%
+
 i =0
 arr = np.array([])
 print(arr)
@@ -65,4 +30,4 @@ print(x_values)
 plt.plot(x_values, arr)
 plt.show()
 
-# %%
+
